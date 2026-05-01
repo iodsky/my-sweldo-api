2da59c6c3eef3c572c986d1ccd2f29bc50f122bd
ibrahim <iharby2002@gmail.com>
2026-05-01 13:53:49 +0800

    refactor: remove JPQL DTO constructors and adopt EmployeeBasic projection interface

--- src/main/java/com/iodsky/mysweldo/employee/EmployeeRepository.java
+++ src/main/java/com/iodsky/mysweldo/employee/EmployeeRepository.java
@@ -12,72 +12,13 @@ import java.util.List;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-    @Query("""
-            SELECT new com.iodsky.mysweldo.employee.EmployeeBasicDto(
-            e.id,
-            e.firstName,
-            e.lastName,
-            new com.iodsky.mysweldo.employee.DepartmentBasicDto(d.id, d.title),
-            new com.iodsky.mysweldo.employee.PositionBasicDto(p.id, p.title),
-            e.status,
-            e.type
-            )
-            FROM Employee e
-            JOIN e.position p
-            JOIN e.department d
-            WHERE e.status = :status
-            """)
-    Page<EmployeeBasicDto> findAllByStatus(@Param("status") EmploymentStatus status, Pageable pageable);
+    Page<EmployeeBasic> findAllByStatus(@Param("status") EmploymentStatus status, Pageable pageable);
 
-    @Query("""
-            SELECT new com.iodsky.mysweldo.employee.EmployeeBasicDto(
-            e.id,
-            e.firstName,
-            e.lastName,
-            new com.iodsky.mysweldo.employee.DepartmentBasicDto(d.id, d.title),
-            new com.iodsky.mysweldo.employee.PositionBasicDto(p.id, p.title),
-            e.status,
-            e.type
-            )
-            FROM Employee e
-            JOIN e.position p
-            JOIN e.department d
-            WHERE d.id = :departmentId
-            """)
-    Page<EmployeeBasicDto> findAllByDepartment_Id(@Param("departmentId") String departmentId, Pageable pageable);
+    Page<EmployeeBasic> findAllByDepartment_Id(@Param("departmentId") String departmentId, Pageable pageable);
 
-    @Query("""
-            SELECT new com.iodsky.mysweldo.employee.EmployeeBasicDto(
-            e.id,
-            e.firstName,
-            e.lastName,
-            new com.iodsky.mysweldo.employee.DepartmentBasicDto(d.id, d.title),
-            new com.iodsky.mysweldo.employee.PositionBasicDto(p.id, p.title),
-            e.status,
-            e.type
-            )
-            FROM Employee e
-            JOIN e.position p
-            JOIN e.department d
-            WHERE e.supervisor.id = :supervisorId
-            """)
-    Page<EmployeeBasicDto> findAllBySupervisor_Id(@Param("supervisorId") Long supervisorId, Pageable pageable);
+    Page<EmployeeBasic> findAllBySupervisor_Id(@Param("supervisorId") Long supervisorId, Pageable pageable);
 
-    @Query("""
-            SELECT new com.iodsky.mysweldo.employee.EmployeeBasicDto(
-            e.id,
-            e.firstName,
-            e.lastName,
-            new com.iodsky.mysweldo.employee.DepartmentBasicDto(d.id, d.title),
-            new com.iodsky.mysweldo.employee.PositionBasicDto(p.id, p.title),
-            e.status,
-            e.type
-            )
-            FROM Employee e
-            JOIN e.position p
-            JOIN e.department d
-            """)
-    Page<EmployeeBasicDto> findAllBasicEmployees(Pageable pageable);
+    Page<EmployeeBasic> findAllBy(Pageable pageable);
 
     @Query("""
         SELECT e.id
